0d2b9373c166936f60f3583cf50fc4f22b95ef01
Sam <30647159+Python-basics@users.noreply.github.com>
2021-02-08 10:31:56 -0600

    Update script.py

--- script.py
+++ script.py
@@ -1,17 +1,17 @@
 
 
+"""
+7) syntax/variable rules
 
 """
-4) Index and Slicing
 
-"""
+# fix what's wrong
 
-word = "CatMat"
+4_test = 5
 
-first =     # assign the first letter of word with indexing
-third =     # assign the third letter of word with indexing
-f_3 =       # assign the first three letters word with slicing
+print("hello)
 
+test 1 = 97
 
 
 
@@ -19,18 +19,7 @@ f_3 =       # assign the first three letters word with slicing
 
 
 def main():
-    if first == "C":
-        print(f"first: passed test, got {first}, expected C")
-    else:
-        print(f"first: failed test, got {first}, expected C")
-    if third == "t":
-        print(f"third: passed test, got {third}, expected t")
-    else:
-        print(f"third: failed test, got {third}, expected t")
-    if f_3 == "Cat":
-        print(f"f_3: passed test, got {f_3}, expected Cat")
-    else:
-        print(f"f_3: failed test, got {f_3}, expected Cat")
+    pass
 
 
 
