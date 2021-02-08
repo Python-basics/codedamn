d05015c46c609512d5d2a1a0973baa93821b3800
Sam <30647159+Python-basics@users.noreply.github.com>
2021-02-08 10:59:46 -0600

    Update script.py

--- script.py
+++ script.py
@@ -1,21 +1,50 @@
-
 """
-11) function and docstring
-
-
+13) Booleans conditions
 """
 
-# create a function goodbye that prints out "goodbye"
-# and has a docstring
+# make the following eqivalent
+
+"""
+eg.
+test = 4 != 3  # I made test Test 4 don't equal 3
+"""
+
+    
+no = 1 ==       # finish equivalence making no True
+
+nope = 3 !=     # finish equivalence making nope False
+
+maybe = 5 >=    # finish equivalence making maybe True
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 def main():
-    goodbye()
+    if no == True:
+        print(f"no: passed test, got {no}, expected True")
+    else:
+        print(f"no: failed test, got {no}, expected True")
+    if nope == False:
+        print(f"nope: passed test, got {nope}, expected False")
+    else:
+        print(f"nope: failed test, got {nope}, expected False")
+    if maybe == True:
+        print(f"maybe: passed test, got {maybe}, expected True")
+    else:
+        print(f"maybe: failed test, got {maybe}, expected True")
 
 
 
 if __name__ == "__main__":
     main()
-
