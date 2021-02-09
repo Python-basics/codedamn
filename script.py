9c6c0059ded62d87aa4303ec61ae4fd7503d1777
Sam <30647159+Python-basics@users.noreply.github.com>
2021-02-09 13:05:07 -0600

    Update script.py

--- script.py
+++ script.py
@@ -35,3 +35,11 @@ def main():
 
 if __name__ == "__main__":
     main()
+  
+'''
+// * Make sure that your code does not crash OUTSIDE of try-catch test blocks, otherwise, no test will pass.
+// * process.env.USER_CODE_DIR is the directory path of user's code. Use it to import/run user specific code 
+// * process.env.PUBLIC_PORT is the publicly accessible port on localhost for user's server. Use it to perform HTTP requests to user server
+// * process.env.UNIT_TEST_OUTPUT_FILE is the name of the file where results of UNIT tests should be put
+// * The results file should have a JSON array with ONLY "true" or "false" values (booleans) as elements having one-to-one correspondance to challenges you design
+'''
