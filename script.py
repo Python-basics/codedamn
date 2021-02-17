e1c2ba574aec7f2358f351b30e49eba4275f58c6
Sam <30647159+Python-basics@users.noreply.github.com>
2021-02-17 16:27:57 -0600

    Update script.py

--- script.py
+++ script.py
@@ -2,19 +2,6 @@
 """
 24)  Custom Function with return
 
-
-
-Finish a function called total...
-You are going to loop through a list
-to total the number of students in a grade
-on the second line write for loop iterating through the variable l (a list)
-on the third line of the body which will be indented
-increment sub_ttl with the intermediate variable you are using
-
-hint: incrementation should be indented under for loop
-
-Great job!!!!
-
 """
 
 
