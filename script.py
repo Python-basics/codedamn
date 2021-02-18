70f1d667dbc46140d9e399462f0a3625fc29ef06
Sam <30647159+Python-basics@users.noreply.github.com>
2021-02-17 21:48:30 -0600

    Update script.py

--- script.py
+++ script.py
@@ -19,23 +19,32 @@ decimal = # float --  float
 
 
 
-def main():
+def main()
+    l = []
+    out = open(os.environ['OUTPUT_PATH'],'w')
     try:
         if type(name) == str:
             print(f"{name} passed test")
+            l.append(True)
         else:
             print(f"{name}: failed test")
+            l.append(False)
         if type(age) == int:
             print(f"{age}: passed test")
+            l.append(True)
         else:
             print(f"{age}: failed test")
+            l.append(False)
         if type(decimal) == float:
             print(f"{decimal}: passed test")
+            l.append(True)
         else:
             print(f"{decimal}: failed test")
+            l.append(False)
     except:
         pass
-
+    out.write(l)
+    out.close()
 if __name__ == "__main__":
     main()
 
