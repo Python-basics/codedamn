e05032b0f32d7dd1aaf0f5c13155b8f56b377118
Sam <30647159+Python-basics@users.noreply.github.com>
2021-02-17 22:52:48 -0600

    Update script.py

--- script.py
+++ script.py
@@ -13,31 +13,31 @@ decimal = # float --  float
 
 
 
-if __name__ == "__main__":
-l = []
-out = open(os.environ['OUTPUT_PATH'],'w')
-try:
-    if type(name) == str:
-        print(f"{name} passed test")
-        l.append(True)
-    else:
-        print(f"{name}: failed test")
-        l.append(False)
-    if type(age) == int:
-        print(f"{age}: passed test")
-        l.append(True)
-    else:
-        print(f"{age}: failed test")
-        l.append(False)
-    if type(decimal) == float:
-        print(f"{decimal}: passed test")
-        l.append(True)
-    else:
-        print(f"{decimal}: failed test")
-        l.append(False)
-    out.write(l)
-    out.close()
-except:
-    pass
+if __name__ == "__main__":  
+    l = []
+    out = open(os.environ['OUTPUT_PATH'],'w')
+    try:
+        if type(name) == str:
+            print(f"{name} passed test")
+            l.append(True)
+        else:
+            print(f"{name}: failed test")
+            l.append(False)
+        if type(age) == int:
+            print(f"{age}: passed test")
+            l.append(True)
+        else:
+            print(f"{age}: failed test")
+            l.append(False)
+        if type(decimal) == float:
+            print(f"{decimal}: passed test")
+            l.append(True)
+        else:
+            print(f"{decimal}: failed test")
+            l.append(False)
+        out.write(l)
+        out.close()
+    except:
+        pass
   
 
