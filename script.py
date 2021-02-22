bf4264199de1996c6614f3905f65e87d1f435943
Sam <30647159+Python-basics@users.noreply.github.com>
2021-02-21 22:41:13 -0600

    Update script.py

--- script.py
+++ script.py
@@ -1,40 +1,11 @@
 """2) variables types
 three type assignment"""
 
-# assign the following types 
-#import os, sys, json, importlib
 
-
-#name =  # str -- string
+name =  # str -- string
 
 #age =   # int -- integer 
 
 #decimal = # float --  float
 
-variable1 = 100
 
-
-import os
-import json
-import sys
-import importlib
-sys.path.append(os.environ.get('USER_CODE_DIR'))
-
-results = []
-
-try:
-    userscript = importlib.import_module('script2')
-    assert userscript.variable1 == 100, "variable1 should be set as 100"
-    # test passed
-    print("Test passed")
-    results.append(True)
-except:
-    # test failed
-    print("Your test failed", sys.exc_info())
-    results.append(False)
-
-
-# append result to file
-f = open(os.environ.get('UNIT_TEST_OUTPUT_FILE'), "w")
-f.write(json.dumps(results))
-f.close()
