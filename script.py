ecb78ef64347a15793619cfefa5e1c801b76fb3e
Sam <30647159+Python-basics@users.noreply.github.com>
2021-04-08 10:54:08 -0500

    Update script.py

--- script.py
+++ script.py
@@ -5,27 +5,31 @@
 
 """
 
-# write an if / else if the following numbers are less than 10
-# uncomment one at a time to test your condition 
+# write an if / else if x is less than 10
+# write an if / else if y greater than 11
+# write an if / else if z is equal to 8
 
 x = 11
-##x = 12
-##x = 8   
+y = 12
+z = 8   
 
-if :  # finish by writing condition here
+if :  # finish by writing x condition here
     print(f"Yes {x} is less than 10")
+    t_1 = False
 else:
     print(f"no {x} is not less than 10")
+    t_1 = True
 
+if :  # finish by writing y condition here
+    print(f"Yes {y} is greater than 11")
+    t_2 = True
+else:
+    print(f"no {y} is not greater than 11")
+    t_2 = False
 
-
-
-
-
-def main():
-    pass
-
-
-
-if __name__ == "__main__":
-    main()
+if :  # finish by writing z condition here
+    print(f"Yes {z} is equal to 8")
+    t_3 = True
+else:
+    print(f"no {z} is not equal to 8")
+    t_3 = False
