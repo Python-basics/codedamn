78ac4cee93990d9162a0fb3310570cca3ea360af
Sam <30647159+Python-basics@users.noreply.github.com>
2021-04-08 09:20:35 -0500

    Update script.py

--- script.py
+++ script.py
@@ -7,11 +7,11 @@
 
 x = 4
 
-x = x   # make x equal 10
+x = x   # add a number to make x equal 10
 
-new = x   # new equal 8
+new = x   # add a number to new to make it equal to 8
 
-new   new   # use incrementing to increase new to 10
+new      # use incrementing to increase new to 10
 
-new   new     # use decrementing to decrease new to 5
+new        # use decrementing to decrease new to 5
 
