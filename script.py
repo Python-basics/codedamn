9370ec646923fed26635e9990315bda090a3a942
Sam <30647159+Python-basics@users.noreply.github.com>
2021-04-08 10:58:48 -0500

    Update script.py

--- script.py
+++ script.py
@@ -6,10 +6,10 @@
 eg. r_1 = 3 % 2 # to assign a remainder of 1
 """
 
-r_2 = 5 %    # enter a number to assign a remainder of 2
+r_2 = 5 %    # enter a number to assign a remainder of 2 to r_2
 
-r_5 = 11 %   # enter a number to assign a remainder of 5
+r_5 = 11 %   # enter a number to assign a remainder of 5 to r_5
 
-r_3 = 10 %   # enter a number to assign a remainder of 3
+r_3 = 10 %   # enter a number to assign a remainder of 3 to r_3
 
 
