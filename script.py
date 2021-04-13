c104eb51b3a4975718dc89d4c2c64386c62c0c5d
Sam <30647159+Python-basics@users.noreply.github.com>
2021-04-13 07:44:52 -0500

    Update script.py

--- script.py
+++ script.py
@@ -7,11 +7,12 @@ iterate through list
 grades = [81,76,96,93,95,100]
 threes = []
 
-# Finish the if condition to only print grades that are divisible 
+# Finish the if condition to only print grades that are evenly divisible by 3
+# hint use % and remainder is 0
 # code below
 
 for grade in grades:
-    if :
+    if : # finish condition here 
         print(grade)
         threes.append(grade)
 
