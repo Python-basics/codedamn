de593832b79e48d31654595e4edaed8144cfeee3
Sam <30647159+Python-basics@users.noreply.github.com>
2021-04-13 10:27:59 -0500

    Update script.py

--- script.py
+++ script.py
@@ -10,7 +10,7 @@
 letters = "abcdefghijk"
 
 
-every_other =           # assign every other letter from letters
+every_second =           # assign every other letter from letters
 
 every_third =           #  assign every 3rd letter from letters
 
@@ -22,25 +22,4 @@ first_3_every_other =   # assign the three letter everyother 1 eg. ace
 
 
 
-
-
-
-def main():
-    if every_other == "acegik":
-        print(f"every_other: passed test, got {every_other} expected acegik")
-    else:
-        print(f"every_other: failed test, got {every_other} expected acegik")
-    if every_third == "adgj":
-        print(f"every_third: passed test, got {every_third} expected acegik")
-    else:
-        print(f"every_other: failed test, got {every_third} expected acegik")
-    if first_3_every_other == "ace":
-        print(f"every_other: passed test, got {first_3_every_other} expected acegik")
-    else:
-        print(f"first_3_every_other: failed test, got {first_3_every_other} expected acegik")
-
-
-
-if __name__ == "__main__":
-    main()
 
