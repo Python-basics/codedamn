a994a4851cd6414a13380813700432cab708fe11
Sam <30647159+Python-basics@users.noreply.github.com>
2021-04-13 13:02:05 -0500

    Update script.py

--- script.py
+++ script.py
@@ -8,6 +8,7 @@
 students = [40,38,24,45,48,32,34]
 
 def total(l):
+    """This function totals the number a students in grade"""
     sub_ttl = 0     # this is your sub-total starting point 
     
 
