1a12533b0e226b623635f16445bbc5f3a4eb649c
Sam <30647159+Python-basics@users.noreply.github.com>
2021-04-13 08:00:00 -0500

    Update script.py

--- script.py
+++ script.py
@@ -11,16 +11,6 @@ grades = [81,76,96,93,95,100]
 
 def divisable_by_3(l):
     # write code below
-
-
-
-
     
 
-def main():
-    divisable_by_3(grades)
 
-
-
-if __name__ == "__main__":
-    main()
