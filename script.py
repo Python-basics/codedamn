f31363514396b9e88418dd1662fa85c13bbd62ee
Sam <30647159+Python-basics@users.noreply.github.com>
2021-04-13 08:38:25 -0500

    Update script.py

--- script.py
+++ script.py
@@ -12,7 +12,11 @@ grades = [81,76,96,93,95,100]
 def divisible_by_3(l):
     
     # write code below, use the code from previous lesson 
+    for grade in grades:
+        if grade % 3 == 0:
+            print(grade)
+            threes.append(grade)
     
 
 
-divisable_by_3(grades)
+print(divisable_by_3(grades))
