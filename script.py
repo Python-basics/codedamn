a27d6daadb9aaf9039a6f9d5f43e24801446274d
Sam <30647159+Python-basics@users.noreply.github.com>
2021-04-14 14:21:41 -0500

    Update script.py

--- script.py
+++ script.py
@@ -5,20 +5,8 @@
 
 two = '2'
 
-string = '15628569'
-
-
 # create a variable converting strings --> integers
 
 # convert variabe to an integer
 n_2 = 
-
-# slice out and convert '15' from string to and integer
-n_15 = 
-
-# slice out and convert '62' from string to and integer
-n_62 = 
-
-# slice out and convert '628' from string to and integer
-n_628 = 
 
