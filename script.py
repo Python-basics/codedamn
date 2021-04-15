08c8712d577b424b4f7ee72c1532d7407528f4b7
Sam <30647159+Python-basics@users.noreply.github.com>
2021-04-15 16:43:02 -0500

    Update script.py

--- script.py
+++ script.py
@@ -7,5 +7,22 @@ x = [5, 7, 0, 8, 6, 3, 2, 1, 4, 9]
 
 # create a variable that uses preforms len on variable x
 
-count_1 = len(x)
 
+sample_1 = "lanfgkjnbiovjnwo"
+
+sample_2 = [9, 5, 6, 2, 13, 17, 1, 19, 3, 0,
+            11, 15, 7, 16, 14, 10, 8, 4, 12, 18]
+
+sample_3 = "                          "
+
+# create variable count_1 to perform len on sample_1
+
+count_1 = 
+
+# create variable count_2 to perform len on sample_2
+
+count_2 = 
+
+# create variable count_3 to perform len on sample_3
+
+count_3 = 
