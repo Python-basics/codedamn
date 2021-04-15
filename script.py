e24a1e34d1e2afa32f38ad73c274887c3cbdaf4e
Sam <30647159+Python-basics@users.noreply.github.com>
2021-04-15 00:30:43 -0500

    Update script.py

--- script.py
+++ script.py
@@ -9,14 +9,14 @@ y = "the great gatsby"
 
 quiet = "yelling"
 
-# create a new variable where the first letter is capitalized
+# create a new variable where the first letter x is capitalized
 
 name = 
 
-# create a new variable where the first letter of each word is capitalized
+# create a new variable where the first letter of each word y is capitalized
 
 movie = 
 
-# create a variable where all letters are upper case
+# create a variable where all letters of quiet are upper case  
 
 yell = 
