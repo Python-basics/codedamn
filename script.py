bdd8a3e4c67472ba4f2a975db0ed8591c114f482
Sam <30647159+Python-basics@users.noreply.github.com>
2021-04-14 23:14:18 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,19 +1,10 @@
 
 """
-27)  Converting
+28)  String Methods
 """
 
-string = '15628569'
+x = "yelling"
 
+# use the upper method to change x "YELLING"
 
-# create a variable converting strings --> integers
-
-# slice out and convert '15' from string to and integer
-n_15 = 
-
-# slice out and convert '62' from string to and integer
-n_62 = 
-
-# slice out and convert '628' from string to and integer
-n_628 = 
-
+yell = 
