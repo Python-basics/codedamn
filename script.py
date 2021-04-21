ea53f64c7628b07ac64079c3d4ff0e8d1c457f39
Sam <30647159+Python-basics@users.noreply.github.com>
2021-04-21 10:54:05 -0500

    Update script.py

--- script.py
+++ script.py
@@ -6,10 +6,10 @@
 
 odd = "13579"
 
-# use indexing to assign the string '5' to the variable five
+# use indexing to assign the string slice '5' to the variable five
 
 five = 
 
-# use indexing to assign the string '1' to the variable one
+# use indexing to assign the string slice '1' to the variable one
 
 one = 
