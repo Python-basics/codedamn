6f88e314f0d23c8ef637811436949e728bb09679
Sam <30647159+Python-basics@users.noreply.github.com>
2021-04-21 16:44:19 -0500

    Update script.py

--- script.py
+++ script.py
@@ -5,12 +5,13 @@
 x = 0
 y = 0
 
+# increment x in the first for loop
+# increment y in the nested(seccond) for loop
+
 for i in range(3):
-    print(x)
-    x += 1
+    # increment x below
     for j in range(3):
-        print(y)
-        y += 1
+       # in crement y below
 
 print(x)
 print(y)
