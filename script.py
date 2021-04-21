fe2cef0b0460437daaf73129f73a9876694f68de
Sam <30647159+Python-basics@users.noreply.github.com>
2021-04-21 13:07:15 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,42 +1,25 @@
 
 """
-31)  len function
+32)  for loops
 """
 
-sample_1 = "lanfgkjnbiovjnwo"
+'''
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+Blastoff!!!!
+'''
 
-sample_2 = [9, 5, 6, 2, 13, 17, 1, 19, 3, 0,
-            11, 15, 7, 16, 14, 10, 8, 4, 12, 18]
+count = 10
 
-sample_3 = "                          "
-
-sample_4 = ''
-
-sample_5 = '              '
-
-sample_6 = '                                                                  '
-
-
-# create variable count_1 to perform len on sample_5
-
-count_1 = 
-
-# create variable count_2 to perform len on sample_2
-
-count_2 = 
-
-# create variable count_3 to perform len on sample_1
-
-count_3 = 
-
-# create variable count_4 to perform len on sample_6
-
-count_4 = 
-
-# create variable count_5 to perform len on sample_3
-
-count_5 = 
-
-# create variable count_6 to perform len on sample_4
-
-count_6 = 
+for i in range(10):
+    print(count)
+    count -= 1
+print("Blastoff!!!!!")
