915b5cec7118c20de7eb7f8364ea1ec6bec952fd
Sam <30647159+Python-basics@users.noreply.github.com>
2021-04-22 13:26:11 -0500

    Update script.py

--- script.py
+++ script.py
@@ -4,9 +4,4 @@
 """
 def number(x):
     """number take a variable return it Zero, Positive or Negative"""
-    if x == 0:
-        return "Zero"
-    elif x > 0:
-        return "Positive"
-    else:
-        return "Negative"
+    # write code below
