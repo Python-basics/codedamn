190118869e94ce67480f6c8c510fe84027fc9532
Sam <30647159+Python-basics@users.noreply.github.com>
2021-04-22 13:11:30 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,24 +1,13 @@
 
 """
-36)  while loop
-10
-9
-8
-7
-6
-5
-4
-3
-2
-1
-Ready or not here I come
-"""
+36)  elif
 
-count = 10
-
-# use decementaion to count down for hide and seek
-while count > 0:
-    print(count)
-    count -= 1
-    
-print("Ready or not here I come")
+def number(x):
+    """number take a variable
+    return it Zero, Positive or Negative"""
+    if x == 0:
+        return "Zero"
+    elif x > 0:
+        return "Positive"
+    else:
+        return "Negative"
