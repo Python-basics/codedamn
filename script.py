c1b1907a10bfe7057bf49bd82c1e9368ffe54627
Sam <30647159+Python-basics@users.noreply.github.com>
2021-04-22 10:51:11 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,14 +1,24 @@
 
 """
 36)  while loop
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+Ready or not here I come
 """
 
 count = 10
 
-# use decementaion to count down
+# use decementaion to count down for hide and seek
 while count > 0:
     print(count)
     count -= 1
     
 print("Ready or not here I come")
-
