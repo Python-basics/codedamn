88717700c5cc6ec1173ff1d3bd3c1941c25731c4
Sam <30647159+Python-basics@users.noreply.github.com>
2021-04-28 13:33:50 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,15 +1,9 @@
 """
-13) Booleans conditions
-
-
-"""
+Booleans conditions
 
 # make the following eqivalent
-
-"""
 eg.
-
-test = 4 != 3  # I made test Test 4 don't equal 3
+test = 4 != 3  --> 4 does not equal 3
 """
 
     
@@ -19,35 +13,3 @@ nope = 3 !=     # finish equivalence making nope False
 
 maybe = 5 >=    # finish equivalence making maybe True
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def main():
-    if no == True:
-        print(f"no: passed test, got {no}, expected True")
-    else:
-        print(f"no: failed test, got {no}, expected True")
-    if nope == False:
-        print(f"nope: passed test, got {nope}, expected False")
-    else:
-        print(f"nope: failed test, got {nope}, expected False")
-    if maybe == True:
-        print(f"maybe: passed test, got {maybe}, expected True")
-    else:
-        print(f"maybe: failed test, got {maybe}, expected True")
-
-
-
-if __name__ == "__main__":
-    main()
