b8aa8ef7fc6404e576ebd7bcdde20c417bd46379
Sam <30647159+Python-basics@users.noreply.github.com>
2021-05-03 16:02:39 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,9 +1,18 @@
 
 """
-38)  Funtion
+40 working with lists
 """
-def area(): # don't forget 2 arguments(variables)
-    """Returns the area of a rectangle"""
-    # code your return code below
 
+l_1 = [2,3,1]
+
+l_2 = ["Sam", "Tom", "Matt"]
+
+
+l_3 = 
+
+Length_1 = 
+
+Length_2 =  
+
+Length_3 = 
 
