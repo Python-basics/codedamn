554790db272ac7e99b1431e4f7f4aa57e9c07f1b
Sam <30647159+Python-basics@users.noreply.github.com>
2021-05-03 16:06:23 -0500

    Update script.py

--- script.py
+++ script.py
@@ -7,12 +7,19 @@ l_1 = [2,3,1]
 
 l_2 = ["Sam", "Tom", "Matt"]
 
+# combine l_1 and l_2 to create l_3
 
 l_3 = 
 
+# find the length of l_2
+
 Length_1 = 
 
+# find the length of l_3
+
 Length_2 =  
+
+# find the length of l_1
 
 Length_3 = 
 
