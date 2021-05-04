7886e6585eb8375a8cefbd9f97c99d7db1d62ddd
Sam <30647159+Python-basics@users.noreply.github.com>
2021-05-04 14:22:40 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,16 +1,28 @@
 
 """
-42 list append method
+43 list index method
 """
 
 
-l_1 = [2,3,1]
+l_1 = [17, 6, 10, 15, 7, 4, 8, 18, 12, 3,
+       2, 19, 1, 9, 11, 14, 13, 0, 16, 5]
 
-l_2 = ["Sam", "Tom", "Matt"]
+l_2 = [9, 0, 12, 10, 15, 16, 5, 7, 8, 13,
+       19, 6, 11, 18, 17, 1, 14, 4, 3, 2]
 
-# append an empty list to l_1
+# find index of 7 in l_1
 
+index_1 = 
 
-# append l_1 to l_2
+# find index of 9 in l_1
 
+index_1 = 
+
+# find index of 1 in l_2
+
+index_1 = 
+
+# find index of 19 in l_2
+
+index_1 = 
 
