0c8efd5259b00d1012637fca763b348da1937bf6
Sam <30647159+Python-basics@users.noreply.github.com>
2021-05-04 20:04:03 -0500

    Update script.py

--- script.py
+++ script.py
@@ -10,17 +10,12 @@ l_1 = [17, 6, 10, 15, 7, 4, 8, 18, 12, 3,
 l_2 = [9, 0, 12, 10, 15, 16, 5, 7, 8, 13,
        19, 6, 11, 18, 17, 1, 14, 4, 3, 2]
 
-# insert 45 at index 5 on l_1
+# sort l_1
 
 
 
-# insert 536 at index 12 on l_2
+# sort l_2
 
 
 
-# insert 6964 at index 17 on l_1
-
-
-
-# insert 1000 at index 8 on l_2
 
