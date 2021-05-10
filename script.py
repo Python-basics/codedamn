df89f907f53cdae97c531fb6ff11e0c56911c198
Sam <30647159+Python-basics@users.noreply.github.com>
2021-05-10 12:25:57 -0500

    Update script.py

--- script.py
+++ script.py
@@ -3,30 +3,30 @@
 47 list count method
 """
 
-l_1 = [17, 6, 1, 8, 1, 12, 3,
-       2, 19, 1, 4, 13, 0, 16, 5,
-      6, 2, 17, 17, 12, 2, 1, 0]
+l_1 = ['o', 'u', 'o', 'a', 'u', 
+       'i', 'o', 'e', 'u', 'o', 
+       'u', 'a', 'u', 'i', 'a']
 
-l_2 = [9, 0, 16, 5, 7, 8, 13,
-       19, 6, 1, 14, 4, 3, 2,
-       2, 19, 1, 4, 16, 5, 7, 8,]
+l_2 =  ['u', 'i', 'i', 'a', 'o', 
+        'u', 'i', 'i', 'u', 'e',
+        'e', 'e', 'i', 'a', 'u']
 
-# count 2 in l_1
+# count 'e' in l_1
 
 count_1 = 
 
-# count 2 in l_2
+# count 'a' in l_2
 
 count_2 = 
 
-# count 1 in l_1
+# count 'e' in l_2
 
 count_3 = 
 
-# count 8 in l_2
+# count 'i' in l_2
 
 count_4 = 
 
-# count 17 in l_1
+# count 'u' in l_1
 
 count_5 = 
