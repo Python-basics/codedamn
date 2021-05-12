4092a63fa76e43ebe7000c3cbd5b966ca999c0f9
Sam <30647159+Python-basics@users.noreply.github.com>
2021-05-12 08:09:17 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,33 +1,28 @@
-
-
 """
 if / else
-
 """
 
-# write an if / else if x is less than 10
-# write an if / else if y greater than 11
 # write an if / else if z is equal to 8
 
 x = 11
 y = 12
 z = 8   
 
-if :  # finish by writing x condition here
+if :  # finish by writing x condition here  if x is less than 10
     print(f"Yes {x} is less than 10")
     t_1 = False
 else:
     print(f"no {x} is not less than 10")
     t_1 = True
 
-if :  # finish by writing y condition here
+if :  # finish by writing y condition here if y greater than 11
     print(f"Yes {y} is greater than 11")
     t_2 = True
 else:
     print(f"no {y} is not greater than 11")
     t_2 = False
 
-if :  # finish by writing z condition here
+if :  # finish by writing z condition here if z is equal to 8
     print(f"Yes {z} is equal to 8")
     t_3 = True
 else:
