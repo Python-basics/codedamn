ea643fd790bc0f3a17d83714242dec56cba2a757
Sam <30647159+Python-basics@users.noreply.github.com>
2021-08-18 13:37:51 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,35 +1,7 @@
 
 """
-List Project
+Dictionaries
 """
 
-letters = "kjasvghougihouoguwysdgngucj"
-
-# create list from letters
-
-l_1 = 
-
-# remove g from l_1
-
-
-
-# what index is i in l_1
-
-index = 
-
-
-# make a copy of  l_1
-
-l_2 = 
-
-# what is the length of l_1
-
-length = 
-
-# count the g's in l_1
-
-count = 
-
-# sort l_1
 
 
