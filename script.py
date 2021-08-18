043fb3c8775e2a971891e3fd55b87f5ac11b63d0
Sam <30647159+Python-basics@users.noreply.github.com>
2021-08-18 14:22:37 -0500

    Update script.py

--- script.py
+++ script.py
@@ -9,7 +9,9 @@ Create 3 dicionaries
 # Create an empty dictionary with variable name d
 
 
+
 # Create a dictionary with a key name and value Sam
+
 
 
 # Create a dictionary with keys [x,y,z,a,b] and values [25,56,1,2,5]
