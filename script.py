548558db9861ddc4dbfeb8e564125e3b0ca4e060
Sam <30647159+Python-basics@users.noreply.github.com>
2021-08-18 19:40:38 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,21 +1,19 @@
 
 """
-Dictionaries
+Dictionaries fromkeys Method
 
-Create 3 dicionaries
+
 """
 
 
-# Create an empty dictionary with variable name d_1
+#
+
+
+# 
 
 
 
-# Create a dictionary with variable name d_2 with a key name and value Sam
-
-
-
-# Create a dictionary with variable name d_3 
-# with keys ['x','y','z','a','b'] and values [25,56,1,2,5]
+# 
 
 
 
