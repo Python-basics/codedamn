4589abc744edacd22933df1c962ac7ecbf9083cf
Sam <30647159+Python-basics@users.noreply.github.com>
2021-08-23 11:05:11 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,19 +1,22 @@
 
 """
 Dictionaries fromkeys Method
-
+Creating dictionaries from iterables
 
 """
 
-
-#
-
-
-# 
+x = [0, 1, 2, 3, 4]
+y = "xyz"
 
 
 
-# 
+# Create a dictionary from x with default value
+
+
+# Create a dictionary from y with default value
+
+
+# Create a dictionary from x with vaules of 0
 
 
 
