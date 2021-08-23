0226025e3f74fe7de371f9ee3b300cb93f862073
Sam <30647159+Python-basics@users.noreply.github.com>
2021-08-23 14:10:37 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 
 """
-Dictionaries Get Method
+Dictionaries Secondary Get Method
 """
 
 d_1 = {'fish': 0, 'dog': 1, 'cat': 2, 'wolf': 3}
@@ -11,15 +11,15 @@ d_3 = {0: {'fish': 0, 'dog': 1, 'cat': 2, 'wolf': 3},
       1: {'w': 0, 'x': 1, 'v': 2, 'y': 3},
       2: {'w': 'fish', 'x': 'dog', 'v': 'cat', 'y': 'wolf'}}
 
-# Use get method to find value for key "v" in d_2
+# Use get method to find value for key "w" in d_2
 
 value_1 = 
 
-# Use get method to find value for key "wolf" in d_1
+# Use get method to find value for key "cat" in d_1
 
 value_2 = 
 
-# Use get method to find value for key 2 in d_3
+# Use get method to find value for key 0 in d_3
 
 value_3 = 
 
