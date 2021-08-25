d53161ed9a899dd0c3b270c62ca9dc2787dc1320
Sam <30647159+Python-basics@users.noreply.github.com>
2021-08-25 11:23:50 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,10 +1,16 @@
 
 """
-Dictionaries Skill Combination
+Dictionaries Pop Method
 
 """
 
 d_1 = {'fish': 0, 'dog': 1, 'cat': 2, 'wolf': 3}
+
+d_2 = {'w': 0, 'x': 1, 'v': 2, 'y': 3}
+
+d_3 = {0: {'fish': 0, 'dog': 1, 'cat': 2, 'wolf': 3},
+      1: {'w': 0, 'x': 1, 'v': 2, 'y': 3},
+      2: {'w': 'fish', 'x': 'dog', 'v': 'cat', 'y': 'wolf'}}
 
 # Create a new dictionary d_2 with keys from d_1 and set all values to 0
 # hint use a temperary variable for keys.  
