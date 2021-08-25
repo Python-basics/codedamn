1004cffa07f1f42bd5cdb52761117396be580a68
Sam <30647159+Python-basics@users.noreply.github.com>
2021-08-25 08:59:28 -0500

    Update script.py

--- script.py
+++ script.py
@@ -6,6 +6,13 @@ Dictionaries Membership
 
 d_1 = {'fish': 0, 'dog': 1, 'cat': 2, 'wolf': 3}
 
+d_2 = {'w': 0, 'x': 1, 'v': 2, 'y': 3}
+
+d_3 = {0: {'fish': 0, 'dog': 1, 'cat': 2, 'wolf': 3},
+      1: {'w': 0, 'x': 1, 'v': 2, 'y': 3},
+      2: {'w': 'fish', 'x': 'dog', 'v': 'cat', 'y': 'wolf'}}
+
+
 keys = []
 
 values = []
