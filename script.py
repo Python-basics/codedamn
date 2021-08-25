b8db4107216adb8b98d99c97d229051582404c62
Sam <30647159+Python-basics@users.noreply.github.com>
2021-08-25 08:23:28 -0500

    Update script.py

--- script.py
+++ script.py
@@ -12,10 +12,7 @@ d_3 = {0: {'fish': 0, 'dog': 1, 'cat': 2, 'wolf': 3},
       1: {'w': 0, 'x': 1, 'v': 2, 'y': 3},
       2: {'w': 'fish', 'x': 'dog', 'v': 'cat', 'y': 'wolf'}}
 
-# Is "fish" a member of d_1
-
 # eg. member_0 = "fish" in d_1
-
 
 # Is "v" a member of d_2
 
