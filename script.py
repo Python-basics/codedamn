df96a57008506462ae05747557b9988f05bb643f
Sam <30647159+Python-basics@users.noreply.github.com>
2021-08-24 23:14:05 -0500

    Update script.py

--- script.py
+++ script.py
@@ -6,26 +6,16 @@ Dictionaries Membership
 
 d_1 = {'fish': 0, 'dog': 1, 'cat': 2, 'wolf': 3}
 
-d_2 = {'w': 0, 'x': 1, 'v': 2, 'y': 3}
+keys = []
 
-d_3 = {0: {'fish': 0, 'dog': 1, 'cat': 2, 'wolf': 3},
-      1: {'w': 0, 'x': 1, 'v': 2, 'y': 3},
-      2: {'w': 'fish', 'x': 'dog', 'v': 'cat', 'y': 'wolf'}}
+values = []
 
-# Is "fish" a member of d_1
-
-# eg. member_0 = "fish" in d_1
+# loop through d_1 a append the keys to the list keys
+# and the values to the list values.  
 
 
-# Is "v" a member of d_2
+# code below here, hint use items method
 
-Member_1 = 
 
-# Is "dog" a member of d_2
 
-Member_2 = 
-
-# Is "fish" a member of d_3[0]
-
-Member_3  = 
 
