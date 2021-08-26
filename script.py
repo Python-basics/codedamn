345b0971c37e7794ad45653b727965dac1c1ba8e
Sam <30647159+Python-basics@users.noreply.github.com>
2021-08-25 20:27:02 -0500

    Update script.py

--- script.py
+++ script.py
@@ -12,14 +12,14 @@ d_3 = {0: {'fish': 0, 'dog': 1, 'cat': 2, 'wolf': 3},
       1: {'w': 0, 'x': 1, 'v': 2, 'y': 3},
       2: {'w': 'fish', 'x': 'dog', 'v': 'cat', 'y': 'wolf'}}
 
-# pop
+# popitem from d_3
 item_1 = 
 
-# pass key "cat" into pop for d_1
+# popitem from d_1
 item_2 = 
 
-# --Warning different-- return 
-# value {'w': 'fish', 'x': 'dog', 'v': 'cat', 'y': 'wolf'} from d_3
-item_3 = 
+# --Warning different--
+# popitem from item_1[1] 
+d = 
 
 
