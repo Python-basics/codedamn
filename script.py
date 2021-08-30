0d4ab9e2dfd300e5e2b50686323ce9eda4557cc4
Sam <30647159+Python-basics@users.noreply.github.com>
2021-08-30 15:03:59 -0500

    Update script.py

--- script.py
+++ script.py
@@ -7,13 +7,14 @@ Dictionaries Setdefault Method
 d_1 = {'fish': 0, 'dog': 1, 'cat': 2, 'wolf': 3}
 
 
-# Use setdefault on key "bear" default value
+# Use setdefault on key "bear" on d_1 with default value
 
 
-# 
+# Use setdefault on key "pig" on d_1 with value 0
 
 
 # --Warning different--
-# 
+# return value_1
+
 
 
