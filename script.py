ce3e48d3b7b04d67fb036da8302de429fff51e25
Sam <30647159+Python-basics@users.noreply.github.com>
2021-08-30 14:57:18 -0500

    Update script.py

--- script.py
+++ script.py
@@ -6,20 +6,14 @@ Dictionaries Setdefault Method
 
 d_1 = {'fish': 0, 'dog': 1, 'cat': 2, 'wolf': 3}
 
-d_2 = {'w': 0, 'x': 1, 'v': 2, 'y': 3}
 
-d_3 = {0: {'fish': 0, 'dog': 1, 'cat': 2, 'wolf': 3},
-      1: {'w': 0, 'x': 1, 'v': 2, 'y': 3},
-      2: {'w': 'fish', 'x': 'dog', 'v': 'cat', 'y': 'wolf'}}
+# Use setdefault on key "bear" default value
 
-# popitem from d_3
-item_1 = 
 
-# popitem from d_1
-item_2 = 
+# 
+
 
 # --Warning different--
-# popitem from item_1[1] 
-item_3 = 
+# 
 
 
