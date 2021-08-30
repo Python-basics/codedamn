0f41575e9499187415e6885ab963ef3abe24fdfb
Sam <30647159+Python-basics@users.noreply.github.com>
2021-08-30 17:14:17 -0500

    Update script.py

--- script.py
+++ script.py
@@ -12,12 +12,14 @@ d_3 = {0: {'fish': 0, 'dog': 1, 'cat': 2, 'wolf': 3},
       1: {'w': 0, 'x': 1, 'v': 2, 'y': 3},
       2: {'w': 'fish', 'x': 'dog', 'v': 'cat', 'y': 'wolf'}}
 
-# Use update on key "dog" on d_1 value 15
+# Use update to add key "t" value 55 to d_2
 
 
-# Use update on key "cat" on d_3[0] value 99
+
+# Use update to add key "duck" value 75 to d_1
 
 
-# Use update on key "x" on d_2 value 111
+
+# Use update to add key 3 value d_1 to d_3
 
 
