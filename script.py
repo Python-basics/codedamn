80f3515b4315f7742ec40b834d4de247493d7855
Sam <30647159+Python-basics@users.noreply.github.com>
2021-08-31 18:01:26 -0500

    Update script.py

--- script.py
+++ script.py
@@ -3,13 +3,12 @@
 Dictionaries Incrementing Dictionary Values
 
 """
+values = [4,8,15,1]
 
 d_1 = {'fish': 0, 'dog': 1, 'cat': 2, 'wolf': 3}
 
-d_2 = {'fish': 5, 'dog': 3, 'cat': 5, 'wolf': 1}
-
-# loop through d_1 and add values from d_2 to d_1's values
-# d_1['wolf'] value would be 4
+# Iterate through d_1 adding contents of list to
+# coresponding value in list
 
 # code below
 
