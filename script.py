e9fc0a40097e8c85d8a23022e2ce2c323be3bd6c
Sam <30647159+Python-basics@users.noreply.github.com>
2021-08-31 13:03:31 -0500

    Update script.py

--- script.py
+++ script.py
@@ -8,9 +8,6 @@ d_1 = {'fish': 0, 'dog': 1, 'cat': 2, 'wolf': 3}
 
 d_2 = {'w': 0, 'x': 1, 'v': 2, 'y': 3}
 
-d_3 = {0: {'fish': 0, 'dog': 1, 'cat': 2, 'wolf': 3},
-      1: {'w': 0, 'x': 1, 'v': 2, 'y': 3},
-      2: {'w': 'fish', 'x': 'dog', 'v': 'cat', 'y': 'wolf'}}
 
 # Use update to add key "t" value 55 to d_2
 
@@ -20,6 +17,6 @@ d_3 = {0: {'fish': 0, 'dog': 1, 'cat': 2, 'wolf': 3},
 
 
 
-# Use update to add key 3 value d_1 to d_3
+# Use update to change key "fish" value to 55  in d_1
 
 
