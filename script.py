0a45b89cfa754db4a171f77b7927a3664bc761e5
Sam <30647159+Python-basics@users.noreply.github.com>
2021-08-31 13:20:56 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 
 """
-Dictionaries Update Method
+Dictionaries Copy Method
 
 """
 
@@ -8,15 +8,17 @@ d_1 = {'fish': 0, 'dog': 1, 'cat': 2, 'wolf': 3}
 
 d_2 = {'w': 0, 'x': 1, 'v': 2, 'y': 3}
 
+d_3 = {0: {'fish': 0, 'dog': 1, 'cat': 2, 'wolf': 3},
+      1: {'w': 0, 'x': 1, 'v': 2, 'y': 3},
+      2: {'w': 'fish', 'x': 'dog', 'v': 'cat', 'y': 'wolf'}}
 
-# Use update to add key "t" value 55 to d_2
-
-
-
-# Use update to add key "duck" value 75 to d_1
-
+# Assign a variable copy_1 from a copy of d_2
 
 
-# Use update to change key "fish" value to 13 in d_1
+# Assign a variable copy_2 from a copy of d_3
+
+
+# Assign a variable copy_3 from a copy of d_1
+
 
 
