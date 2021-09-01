2ef4840faf0aa3508d16debcd07182cd3e5c8778
Sam <30647159+Python-basics@users.noreply.github.com>
2021-09-01 11:20:50 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,14 +1,15 @@
 
 """
-Dictionaries Incrementing Dictionary Values
+String Assignment
 
 """
-values = [4,8,15,1]
 
-d_1 = {'fish': 0, 'dog': 1, 'cat': 2, 'wolf': 3}
+# assign string "happy" to s_1
 
-# Iterate through d_1 adding contents of list to
-# coresponding value in list
 
-# code below
+# assign string "running" to s_2
+
+
+# assign string "jumping" to s_3
+
 
