f53333acaa1ab05beb12098c23368f1c923219a7
Sam <30647159+Python-basics@users.noreply.github.com>
2021-09-01 17:14:37 -0500

    Update script.py

--- script.py
+++ script.py
@@ -3,18 +3,18 @@
 String Index
 """
 
-words = ["kghs", "ajljbhsha","oyhkljhalhhshiuhu"]
+letters = 'abdfgrtafb'
 
-checks = ["hs","jbhs","hkljh"]
-
-# code below 
+# Find the index the first index of 'a' in string letters
 
 index_1 = 
 
+# Find the index the first index of 'b' in string letters
 
-index_2
+index_2 = 
 
+# Find the index the second index of 'a' in string letters
 
-index_3
+index_3 = 
 
 
