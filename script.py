fea640e46c59f6704d1776ef1d9b56073c975af8
Sam <30647159+Python-basics@users.noreply.github.com>
2021-09-07 09:32:40 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,27 +1,20 @@
 
 """
-String Indexing Custom Function
+String Count 
 """
 
+letters = 'abdftgrtaftb'
 
-# 
+# Find the count of sub-string 'a' in string letters
 
+count_1 = 
 
+# Find the count of sub-string 'b' in string letters
 
+count_2 = 
 
-letters = 'abdfgrtafb'
+# Find the count of sub-string 'ft' in string letters
 
-# Find the index the first index of 'a' in string letters
-
-index_1 = 
-
-# Find the index the first index of 'b' in string letters
-
-index_2 = 
-
-# Find the index the second index of 'a' in string letters
-# hint you'll need a starting point
-
-index_3 = 
+count_3 = 
 
 
