a9b69c605e5bb7aed3b3df14cbb6ca7fb5ac65f9
Sam <30647159+Python-basics@users.noreply.github.com>
2021-09-08 08:16:40 -0500

    Update script.py

--- script.py
+++ script.py
@@ -5,13 +5,15 @@ String find method
 
 letters = "ghilfsovlpsf"
 
-# 
+# use a find "lfs" in letters
 
 find_1 = 
 
+# use a find "sov" in letters
   
 find_2 = 
 
+# use a find "pp" in letters
 
 find_3 =
   
