ba4d615f8ec16ecb54f5bc1985239f36fd19e07b
Sam <30647159+Python-basics@users.noreply.github.com>
2021-09-08 13:20:11 -0500

    Update script.py

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ String find function
 """
 # using to find method to create a function that
 # returns index location if sub-string is present
-# or returns "not here" if sub-string is not present
+# or returns "Not Here" if sub-string is not present
 
 
 def location(letters,sub):
