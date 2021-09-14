992f13d24746e123f90885a1c3c3f015335dd48a
Sam <30647159+Python-basics@users.noreply.github.com>
2021-09-14 11:35:21 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,12 +1,14 @@
 
 """
-String Rindex
+String First / Last Index
 """
+# Create a function that returns 
+# list of the first and last occerance
+# of a sub-string 
+# Assume sub-string is present
+# you can use index methods
 
-sen = "  Movies  Tonight   "
 
-# Strip variable sen
-
-strp  = 
-
+def first_last(letters):
+    
 
