c75353eb3850f85d014ddf4dba01492054a9d4a0
Sam <30647159+Python-basics@users.noreply.github.com>
2021-09-16 12:51:24 -0500

    Update script.py

--- script.py
+++ script.py
@@ -7,7 +7,7 @@ String Padding "_"
 # are going to have leading "_" no more than 3
 # eg. _Peter  __Matt  ___Sam
 
-# Create that pads upto 3 _ preciding
+# Create function that pads upto 3 _ leading
 
 def Pre_padding(word):
     
