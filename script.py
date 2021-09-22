2ffbea1f55ba864c53cd37e8e2592b42fa0d0a07
Sam <30647159+Python-basics@users.noreply.github.com>
2021-09-22 08:04:33 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,12 +1,11 @@
 
 """
-Max and Min
+Absolute Value
 """
 
 # Create a function that
-# returns a list of the max and min of a
-# dictionay values
-# eg. [max(),min()]
+# returns an absolute value of even values
+# eg. -2 -> 2 -3 -> -3
 
-def Max_Min(Dict):
+def abs_even(num):
     
