6505de3e79f930ccce205c11838fb622565d9085
Sam <30647159+Python-basics@users.noreply.github.com>
2021-09-22 08:17:20 -0500

    Update script.py

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ Absolute Value
 # Create a function that calculates the 
 # sum of a list the returns 
 # absolute value of even values
-# eg. -2 -> 2 -3 -> -3
+# eg. -2 -> 2 : -3 -> -3
 
-def abs_even(num):
+def abs_even(nums):
     
