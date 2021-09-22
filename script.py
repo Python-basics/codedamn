f7902513e059cba3c9d8d69a96bb9acf6f52ba26
Sam <30647159+Python-basics@users.noreply.github.com>
2021-09-22 14:02:35 -0500

    Update script.py

--- script.py
+++ script.py
@@ -6,6 +6,7 @@ Rounding Grade Points
 # Create a function that takes a list
 # calculates a students grade then rounds
 # a students grade to nearest hundredth
+# eg. 78.32
 
 
 def round_grade(grades):
