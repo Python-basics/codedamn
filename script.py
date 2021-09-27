113590152e0a11750145bff657e6b389e767d23c
Sam <30647159+Python-basics@users.noreply.github.com>
2021-09-26 23:05:01 -0500

    Update script.py

--- script.py
+++ script.py
@@ -6,6 +6,6 @@ Number a List
 # Create a function that returns a
 # numbered list with a starting point
 
-def number(l,start):
+def numbered(l,start):
     """Take l and start numbering at start"""
     
