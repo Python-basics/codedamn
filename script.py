9d95af83c408db91a46ecb6e326253cf09b78b16
Sam <30647159+Python-basics@users.noreply.github.com>
2021-09-26 23:42:15 -0500

    Update script.py

--- script.py
+++ script.py
@@ -4,9 +4,14 @@ Number a List
 """
 
 # Create a function that returns a
-# enumerate object from the starting point
-# which is given
+# dictionary of all accurances of a
+# word in a poem
 
-def numbered(l,start):
-    """Take l and start numbering with start argument"""
+# importing the text of the poem raven
+# to variable text
+from raven import text
+
+
+def Count(poem):
+    """Count the words in a poem"""
     
