f075dd9b79cae3b3c587598ce3e4b7b51355ef76
Sam <30647159+Python-basics@users.noreply.github.com>
2021-09-26 21:16:06 -0500

    Update script.py

--- script.py
+++ script.py
@@ -6,6 +6,11 @@ Sort Lists
 # Create a function that sorts a list 
 # of lists by 0,1 or 2 index
 
+def index(l,place):
+    """l for list and place for index"""
 
+    
+    
 def sort_index(l_o_l,place):
+    """Use the helper function"""
     
