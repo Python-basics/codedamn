060a9664c716a444170877ef01731a27456d745d
Sam <30647159+Python-basics@users.noreply.github.com>
2021-09-28 23:11:47 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,18 +1,21 @@
 
 """
-Counting Words
+Accending Order
 """
 
 # Create a function that returns a
-# list of the top 5 words (in accending order)
-# used and the number of occerances
-# eg. [('the', 56)]
+# a string of the word from The Raven
+# that occur 7 times, reorders of the 
+# characters in alphabetical order
+# eg. ['in', 'with', 'no', 'or', 'above', 'from', 'his']
+# take those characers place them in order and 
+# join as a single string
 
 # importing the text of the poem raven
 # to variable text
 
 from raven import text
 
-def counting(poem):
-    """Count the words in a poem"""
+def alpha_order_7(poem):
+    """alpha order string"""
     
