2124b676e151b0c0bd992160dc102762ed11e0c2
Sam <30647159+Python-basics@users.noreply.github.com>
2021-10-20 21:20:14 -0500

    Update script.py

--- script.py
+++ script.py
@@ -14,6 +14,6 @@ n_15 =
 # slice out and convert '62' from string to and integer
 n_62 = 
 
-# slice out and convert '628' from string to and integer
+# slice out and convert '2856' from string to and integer
 n_2856 = 
 
