783770c5d2e8caf987b8451b6edce3e7a8215cda
Sam <30647159+Python-basics@users.noreply.github.com>
2021-11-24 13:01:21 -0600

    Update script.py

--- script.py
+++ script.py
@@ -6,11 +6,11 @@ Dictionaries Skill Combination
 
 d_1 = {'fish': 0, 'dog': 1, 'cat': 2, 'wolf': 3}
 
-# Create a new dictionary d_2 with keys from d_1 and set all values to 0
-# hint use a temperary variable for keys.  
+# Warp dictionary methods in a list funciton
 
+keys = 
 
-d_2 = 
+values =
 
 
 
