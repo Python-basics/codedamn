1802f3bfdd1ab42f9cf02cba1322d8b4694e7b0b
Sam <30647159+Python-basics@users.noreply.github.com>
2021-11-23 20:21:53 -0600

    Update script.py

--- script.py
+++ script.py
@@ -11,7 +11,7 @@ d_3 = {0: {'fish': 0, 'dog': 1, 'cat': 2, 'wolf': 3},
       1: {'w': 0, 'x': 1, 'v': 2, 'y': 3},
       2: {'w': 'fish', 'x': 'dog', 'v': 'cat', 'y': 'wolf'}}
 
-# Create a new dictionary of keys from d_3 and value from d_2['w']
+# Create a new dictionary of keys from d_3 and value from key 'w' of d_2
 
 d_4 = 
 
