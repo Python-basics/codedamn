992cda288b4749cdeba4bc7ee60b60da68aad788
Sam <30647159+Python-basics@users.noreply.github.com>
2021-12-01 21:43:52 -0600

    Update script.py

--- script.py
+++ script.py
@@ -7,6 +7,9 @@ String Startswith Function
 # prefixes and words return a list if the word 
 # in words list containes either of the prefixes
 # hint use nested loops
+# eg. prefixes = ['d','e'] words = ['hat','dog','elf','dot']
+# return ['dog','elf','dot']
+
 
 def either(prefixes,words):
     
