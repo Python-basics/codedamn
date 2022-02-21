f22e6dcac04b98b0a161826cf9425fb5040a38b6
Sam <30647159+Python-basics@users.noreply.github.com>
2022-02-21 15:57:01 -0600

    Update script.py

--- script.py
+++ script.py
@@ -4,7 +4,11 @@ String Convert to Past Tense
 """
 
 # Create a function to change to past tense 
-# as needed
+# as needed eg; 
+# jump --> jumped add "ed" 
+# move --> moved  add "d"
+# walked --> walked and nothing 
+
 
 def Past_tense(word):
     
